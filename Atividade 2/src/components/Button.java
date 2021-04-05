e0a81f90544d331f9dc358811c7651c7a3b22e57
Rogério Luiz <rogerio.macdev@gmail.com>
2021-04-05 19:35:06 -0300

    ✨ New Button Component

---
+++ Atividade 2/src/components/Button.java
@@ -0,0 +1,46 @@
+package components;
+
+import javax.swing.*;
+
+public class Button {
+    private String title;
+    private int width;
+    private int height;
+    private int x;
+    private int y;
+    private JButton b;
+
+    public Button(String title, int width, int height, int x, int y) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.b = new JButton(title);
+        this.b.setBounds(x, y, width, height);
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+    
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public JButton render() {
+        return this.b;
+    }
+}
